4f9bba370dab59364c6fded7151bc1b54d535d50
Himanshu Sharma <hsharma@craterzone.com>
2018-01-31 12:09:03 +0530

    six forking

---
+++ src/main/java/com/test/fork/master/Six.java
@@ -0,0 +1,24 @@
+package com.test.fork.master;
+
+public class First {
+	
+	
+public void testFirst(){
+	System.out.println("First Commit ");
+}
+
+
+public void testSecond(){
+	System.out.println("Second Commit ");
+}
+
+
+	
+public static void main(String[] args) {
+	First f=new First();
+	f.testFirst();
+	
+	System.out.println("First Commit ");
+}
+
+}
