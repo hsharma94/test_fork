341541b50a26b9248700fa56d3d4a77b3480615e
Himanshu Sharma <hsharma@craterzone.com>
2018-01-31 10:44:53 +0530

    first method addded

---
+++ src/main/java/com/test/fork/master/First.java
@@ -0,0 +1,17 @@
+package com.test.fork.master;
+
+public class First {
+	
+	
+public void testFirst(){
+	System.out.println("First Commit ");
+}
+	
+public static void main(String[] args) {
+	First f=new First();
+	f.testFirst();
+	
+	System.out.println("First Commit ");
+}
+
+}
